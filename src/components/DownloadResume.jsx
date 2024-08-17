97e66097686074704000bd34ddcbbde68cd46469
hira84 <166253407+hira84@users.noreply.github.com>
2024-08-17 13:20:29 -0400

    update

--- src/components/DownloadResume.jsx
+++ src/components/DownloadResume.jsx
@@ -1,9 +1,10 @@
 import { FaDownload } from "react-icons/fa";
+import resume from "../components/Hira-Resume.pdf"
 
 const DownloadResume = () => {
   return (
     // Replace the href with the a link to your resume and replace the download attribute with your name
-    <a href="components/Hira-Resume.pdf" download="Hira-Resume.pdf" className="flex items-center">
+    <a href={resume} download="Hira-Resume.pdf" className="flex items-center">
       <p className="mr-2">Download Resume</p>
       <FaDownload />
     </a>
